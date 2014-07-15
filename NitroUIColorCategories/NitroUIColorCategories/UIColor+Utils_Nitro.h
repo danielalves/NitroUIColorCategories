d9c419ec8e9790334eb22e6006e359101bdf4e61
Daniel L. Alves <alves.lopes.dan@gmail.com>
2014-07-15 17:48:34 -0300

    Documenting component getters and luminance utilities

--- NitroUIColorCategories/NitroUIColorCategories/UIColor+Utils_Nitro.h
+++ NitroUIColorCategories/NitroUIColorCategories/UIColor+Utils_Nitro.h
@@ -44,19 +44,126 @@ FOUNDATION_EXPORT NSString * const COLOR_DICT_COMPONENT_KEY_ALPHA;
 +( UIColor * )colorWithRGBAHexString:( NSString * )rgbaStr;
 +( UIColor * )colorWithARGBHexString:( NSString * )argbStr;
 
-// Component getters
+/*********************************
+ *
+ * Component getters
+ *
+ ********************************/
+
+/**
+ *  Returns the red component of the color as float value in the interval [0.0, 1.0].
+ *  Since this method does not receive parameters, it is possible to call it as if it 
+ *  was a getter. Example:
+ *
+ *  CGFloat r = color.red;
+ *
+ *  @return The red component of the color.
+ */
 -( CGFloat )red;
+
+/**
+ *  Returns the green component of the color as float value in the interval [0.0, 1.0].
+ *  Since this method does not receive parameters, it is possible to call it as if it
+ *  was a getter. Example:
+ *
+ *  CGFloat g = color.green;
+ *
+ *  @return The green component of the color.
+ */
 -( CGFloat )green;
+
+/**
+ *  Returns the blue component of the color as float value in the interval [0.0, 1.0].
+ *  Since this method does not receive parameters, it is possible to call it as if it
+ *  was a getter. Example:
+ *
+ *  CGFloat b = color.blue;
+ *
+ *  @return The blue component of the color.
+ */
 -( CGFloat )blue;
+
+/**
+ *  Returns the alpha component of the color as float value in the interval [0.0, 1.0].
+ *  Since this method does not receive parameters, it is possible to call it as if it
+ *  was a getter. Example:
+ *
+ *  CGFloat a = color.alpha;
+ *
+ *  @return The alpha component of the color.
+ */
 -( CGFloat )alpha;
 
+/**
+ *  Returns the red component of the color as float uint8_t in the interval [0, 255].
+ *  Since this method does not receive parameters, it is possible to call it as if it
+ *  was a getter. Example:
+ *
+ *  uint8_t r = color.red;
+ *
+ *  @return The red component of the color.
+ */
 -( uint8_t )byteRed;
+
+/**
+ *  Returns the green component of the color as uint8_t value in the interval [0, 255].
+ *  Since this method does not receive parameters, it is possible to call it as if it
+ *  was a getter. Example:
+ *
+ *  uint8_t g = color.green;
+ *
+ *  @return The green component of the color.
+ */
 -( uint8_t )byteGreen;
+
+/**
+ *  Returns the blue component of the color as uint8_t value in the interval [0, 255].
+ *  Since this method does not receive parameters, it is possible to call it as if it
+ *  was a getter. Example:
+ *
+ *  uint8_t b = color.blue;
+ *
+ *  @return The blue component of the color.
+ */
 -( uint8_t )byteBlue;
+
+/**
+ *  Returns the alpha component of the color as uint8_t value in the interval [0, 255].
+ *  Since this method does not receive parameters, it is possible to call it as if it
+ *  was a getter. Example:
+ *
+ *  uint8_t a = color.alpha;
+ *
+ *  @return The alpha component of the color.
+ */
 -( uint8_t )byteAlpha;
 
-// Luminance
+/*********************************
+ *
+ * Luminance
+ *
+ ********************************/
+
+/**
+ *  The luminance of the color as float value in the interval [0.0, 1.0].
+ *  Since this method does not receive parameters, it is possible to call it as if it
+ *  was a getter. Example:
+ *
+ *  CGFloat luminance = color.luminance;
+ *
+ *  @return The luminance of the color.
+ */
 -( CGFloat )luminance;
+
+/**
+ *  The luminance of the color as uint8_t value in the interval [0, 255].
+ *  Since this method does not receive parameters, it is possible to call it as if it
+ *  was a getter. Example:
+ *
+ *  uint8_t luminance = color.luminance;
+ *
+ *  @return The luminance of the color.
+ */
 -( uint8_t )byteLuminance;
 
 // Conversion
